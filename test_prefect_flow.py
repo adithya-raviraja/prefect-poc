c6f73b9ab80e9c15e06cb54a474582cfb08e1338
Adithya Raviraja <Adithya.raviraj96@gmail.com>
2025-04-11 13:03:49 +0530

    Update test_prefect_flow.py

--- test_prefect_flow.py
+++ test_prefect_flow.py
@@ -1,2 +1,8 @@
+from prefect import flow
+
+@flow
+def test_functions():
+    print("HELLO FROM THE FLOW FUNCTION")
+
 if __name__ == "__main__":
-  print('hello from main')
+    print('hello from main')
