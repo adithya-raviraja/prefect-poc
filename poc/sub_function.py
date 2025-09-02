96231740990defea7ccab7be441cf44cd2d6d321
Adithya Raviraja <adithyaraviraja@mastersindia.co>
2025-09-02 17:58:49 +0530

    test

--- poc/sub_function.py
+++ poc/sub_function.py
@@ -4,6 +4,6 @@ from prefect import task, get_run_logger
 @task
 def sleep_for_a_while(seconds: int):
     logger = get_run_logger()
-    logger.info(f"Sleeping for {seconds} seconds...")
+    logger.info(f"Beginning Sleeping for {seconds} seconds...")
     sleep(seconds)
-    logger.info(f"Slept for {seconds} seconds")
+    logger.info(f"Completed Slept for {seconds} seconds")
